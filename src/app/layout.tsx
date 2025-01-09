1684ace8c0185ee679c8e910e2c4f2a85d31041f
EKOUE jeremie <jeremiekoue8@gmail.com>
2025-01-09 23:27:04 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next";
 import "@/styles/globals.css";
-import { satoshi } from "@/styles/fonts";
+import { integralcf } from "@/styles/fonts";
 import TopBanner from "@/components/layout/Banner/TopBanner";
 import TopNavbar from "@/components/layout/Navbar/TopNavbar";
 import Footer from "@/components/layout/Footer";
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={satoshi.className}>
+      <body className={integralcf.className}>
         <HolyLoader color="#868686" />
         <TopBanner />
         <Providers>
